38ac83c550767cc5071e9ac7e28a95c15420158f
Marcelo Barboza <salve.barboza@pm.me>
2022-06-22 23:19:23 -0300

    change border width to 5 units

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Fira Code:pixelsize=20:antialias=true:autohint=true";
-static int borderpx = 10;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
