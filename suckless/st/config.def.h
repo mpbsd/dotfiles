68d1b758102fb4f1fbf2f651139888f9fbabeb47
Marcelo Barboza <salve.barboza@pm.me>
2022-07-08 18:10:33 -0300

    use ubuntu mono fonts on terminal

--- suckless/st/config.def.h
+++ suckless/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
