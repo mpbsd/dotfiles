bea929a86ee64c0eee6344f384ccb7ac6ee92490
Marcelo Barboza <salve.barboza@pm.me>
2022-07-26 12:26:45 -0300

    organizing my nvim config in a more consistent way
      keeping the various plugins' configurations in a single file. It makes
      sense since, for me at least, I have a very minimal set of plugins and
      I make almost no midifications to their defaults =)

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,11 +5,7 @@ kmap('i', 'jj', '<Esc>', opts)
 
 kmap('n', '<Leader>;', ':', opts)
 
--- file commands
-kmap('n', '<Leader>fw', ':w<CR>', opts)
-kmap('n', '<Leader>fq', ':q<CR>', opts)
-
--- edit and source neovim config files
+-- edit/source neovim config files
 kmap('n', '<Leader>erc', ':edit $MYVIMRC<CR>', opts)
 
 kmap('n', '<Leader>eop', ':edit ~/.config/nvim/lua/options.lua<CR>', opts)
@@ -21,6 +17,10 @@ kmap('n', '<Leader>src', ':source $MYVIMRC<CR>', opts)
 kmap('n', '<Leader>sop', ':source ~/.config/nvim/lua/options.lua<CR>', opts)
 kmap('n', '<Leader>skm', ':source ~/.config/nvim/lua/keymaps.lua<CR>', opts)
 kmap('n', '<Leader>spg', ':source ~/.config/nvim/lua/plugins.lua<CR>', opts)
+
+-- file commands
+kmap('n', '<Leader>fw', ':w<CR>', opts)
+kmap('n', '<Leader>fq', ':q<CR>', opts)
 
 -- window navigation
 kmap('n', '<C-h>', '<C-w>h', opts)
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -50,8 +50,41 @@ vim.g.UltiSnipsEditSplit           = 'horizontal'
 
 vim.g.vimwiki_list = {{ path = '~/projects/vimwiki' }}
 
--- telescope
+-- -- telescope
 kmap('n', '<Leader>ff', "<Cmd>lua require('telescope.builtin').find_files()<CR>", opts)
 kmap('n', '<Leader>fg', "<Cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
 kmap('n', '<Leader>fb', "<Cmd>lua require('telescope.builtin').buffers()<CR>", opts)
 kmap('n', '<Leader>fh', "<Cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
+
+-- treesitter
+require 'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "latex", "lua", "markdown", "python" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for
+    -- example if you want to disable highlighting for the `tex` filetype, you
+    -- need to include `latex` in this list as this is the name of the parser)
+    -- list of language that will be disabled
+    disable = { "javscript" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same
+    -- time. Set this to `true` if you depend on 'syntax' being enabled (like
+    -- for indentation). Using this option may slow down your editor, and you
+    -- may see some duplicate highlights. Instead of true it can also be a list
+    -- of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
