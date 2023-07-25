b7b8338af6163144fddc81dd9bd4bc4a8070189c
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-07-25 00:53:04 -0300

    fix mental confusion
    those lsp keybindings went all the way up into my treesitter
    configuration. Fixed just in time.

---
+++ nvim/.config/nvim/lua/mpbsd/plugins/lsp.lua
@@ -0,0 +1,74 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "j-hui/fidget.nvim",
+      tag = "legacy",
+      opts = {},
+    }
+  },
+  config = function()
+    -- Setup language servers.
+    local lspconfig = require("lspconfig")
+    lspconfig.pyright.setup {
+      cmd = { "pyright-langserver", "--stdio" },
+      filetypes = { "python" },
+      -- root_dir = "see source file",
+      settings = {
+        ["pyright"] = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "workspace",
+              useLibraryCodeForTypes = true
+            }
+          }
+        }
+      },
+      single_file_support = true,
+    }
+    lspconfig.rust_analyzer.setup {
+      -- Server-specific settings. See `:help lspconfig-setup`
+      settings = {
+        ["rust-analyzer"] = {},
+      },
+    }
+    -- Global mappings.
+    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, { desc = "Open diagnostic list" })
+    -- Use LspAttach autocommand to only map the following keys
+    -- after the language server attaches to the current buffer
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<space>wl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<space>f", function()
+          vim.lsp.buf.format { async = true }
+        end, opts)
+      end,
+    })
+  end
+}
+
+-- vim: ts=2 sts=2 sw=2 et
