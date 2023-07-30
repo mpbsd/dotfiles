efc9a13c3136259b81f297ee8fb7eaa670750bcd
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-07-29 22:50:11 -0300

    add dracula colorscheme also to my neovim config

--- nvim/.config/nvim/lua/mpbsd/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/mpbsd/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
+  "Mofiqul/dracula.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    vim.cmd.colorscheme("tokyonight-night")
+    vim.cmd.colorscheme("dracula-soft")
   end
 }
 
