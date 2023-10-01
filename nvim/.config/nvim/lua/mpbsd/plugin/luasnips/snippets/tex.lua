eb497f3c1cd8153990f9b3b3badd9fb9ee4a1e36
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-10-01 15:00:10 -0300

    add upper case greek letter

--- nvim/.config/nvim/lua/mpbsd/plugin/luasnips/snippets/tex.lua
+++ nvim/.config/nvim/lua/mpbsd/plugin/luasnips/snippets/tex.lua
@@ -48,8 +48,58 @@ local M = {
     -- }}}
   },
   -- }}}
+  -- upper case greek letter {{{
+  -- lhs {{{
+  upper_case_greek_letter = {
+    lhs = {
+      trig        = "([ABGDEZHJIKLMNXOPRSTUFQYW])`",
+      dscr        = "upper case greek letter",
+      regTrig     = true,
+      wordTrig    = false,
+      snippetType = "autosnippet",
+    },
+    -- }}}
+    -- rhs {{{
+    rhs = {
+      f(function(_, snip)
+        -- alphabet {{{
+        local alphabet = {
+          A = "\\Alpha",
+          B = "\\Beta",
+          G = "\\Gamma",
+          D = "\\Delta",
+          E = "\\Epsilon",
+          Z = "\\Zeta",
+          H = "\\Eta",
+          J = "\\Theta",
+          I = "\\Iota",
+          K = "\\Kappa",
+          L = "\\Lambda",
+          M = "\\Mu",
+          N = "\\Nu",
+          X = "\\Xi",
+          O = "\\Omicron",
+          P = "\\Pi",
+          R = "\\Rho",
+          S = "\\Sigma",
+          T = "\\Tau",
+          U = "\\Upsilon",
+          F = "\\Phi",
+          Q = "\\Chi",
+          Y = "\\Psi",
+          W = "\\Omega",
+        }
+        -- }}}
+        return alphabet[snip.captures[1]]
+      end
+      )
+    }
+    -- }}}
+  },
+  -- }}}
 }
 
 return {
   s(M.lower_case_greek_letter.lhs, M.lower_case_greek_letter.rhs),
+  s(M.upper_case_greek_letter.lhs, M.upper_case_greek_letter.rhs),
 }
