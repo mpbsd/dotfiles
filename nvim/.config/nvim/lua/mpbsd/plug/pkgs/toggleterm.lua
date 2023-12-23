eb04274e1c6a69cd04861b4d1fc04d21e43a8ccf
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-12-23 18:43:31 -0300

    delete 'playground' plugin for neovim as it's now deprecated
    because neovim ships this functionality by default

--- nvim/.config/nvim/lua/mpbsd/plug/pkgs/toggleterm.lua
+++ nvim/.config/nvim/lua/mpbsd/plug/pkgs/toggleterm.lua
@@ -2,11 +2,11 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      open_mapping = [[<C-\>]],
+      open_mapping    = [[<C-\>]],
       start_in_insert = true,
-      autochdir = true,
-      direction = "float",
-      close_on_exit = true,
+      autochdir       = true,
+      direction       = "float",
+      close_on_exit   = true,
     })
   end
 }
