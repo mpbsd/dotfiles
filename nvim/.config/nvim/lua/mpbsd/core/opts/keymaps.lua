89b2bdc7a4830d5a6565ba6561e26653bbdf9b3a
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-12-23 21:07:15 -0300

    use Keymap once more

--- nvim/.config/nvim/lua/mpbsd/core/opts/keymaps.lua
+++ nvim/.config/nvim/lua/mpbsd/core/opts/keymaps.lua
@@ -104,10 +104,4 @@ local keymaps = {
 }
 
 
-for _, keymap in pairs(keymaps) do
-  local mod = keymap["mod"]
-  local lhs = keymap["lhs"]
-  local rhs = keymap["rhs"]
-  local opt = keymap["opt"]
-  vim.keymap.set(mod, lhs, rhs, opt)
-end
+Keymap(keymaps)
