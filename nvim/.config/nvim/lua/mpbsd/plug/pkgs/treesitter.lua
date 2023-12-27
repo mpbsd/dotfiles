a60001bc383c724a976a36a432a6c5d822764cc3
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-12-27 07:08:49 -0300

    some tests

--- nvim/.config/nvim/lua/mpbsd/plug/pkgs/treesitter.lua
+++ nvim/.config/nvim/lua/mpbsd/plug/pkgs/treesitter.lua
@@ -63,24 +63,24 @@ return {
           enable = true,
           set_jumps = true, -- whether to set jumps in the jumplist
           goto_next_start = {
-            ["]m"] = "@function.outer",
             ["]]"] = "@class.outer",
             ["]b"] = "@block.outer",
+            ["]m"] = "@function.outer",
           },
           goto_next_end = {
-            ["]M"] = "@function.outer",
             ["]["] = "@class.outer",
             ["]B"] = "@block.outer",
+            ["]M"] = "@function.outer",
           },
           goto_previous_start = {
-            ["[m"] = "@function.outer",
             ["[["] = "@class.outer",
             ["[b"] = "@block.outer",
+            ["[m"] = "@function.outer",
           },
           goto_previous_end = {
-            ["[M"] = "@function.outer",
             ["[]"] = "@class.outer",
             ["[B"] = "@block.outer",
+            ["[M"] = "@function.outer",
           },
         },
         swap = {
