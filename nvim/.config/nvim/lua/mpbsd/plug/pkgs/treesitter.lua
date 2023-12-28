7fd86872071decb1a9c0ca4132987b60394e0328
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-12-28 11:27:11 -0300

    format the use of a function

--- nvim/.config/nvim/lua/mpbsd/plug/pkgs/treesitter.lua
+++ nvim/.config/nvim/lua/mpbsd/plug/pkgs/treesitter.lua
@@ -28,7 +28,10 @@ return {
           disable = function(_, buf)
             local globals = require("mpbsd.core.opts.globals")
             local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(globals.vim_uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+            local ok, stats = pcall(
+              globals.vim_uv.fs_stat,
+              vim.api.nvim_buf_get_name(buf)
+            )
             if ok and stats and stats.size > max_filesize then
               return true
             end
