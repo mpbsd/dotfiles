d91023dbb9436444f8d0acb8dfa8e87f2b6ff186
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2023-12-30 15:56:47 -0300

    slowly change rsa -> ed25519

--- zsh/.config/zsh/rc/keychain.zsh
+++ zsh/.config/zsh/rc/keychain.zsh
@@ -1,4 +1,4 @@
-keychain --nogui id_rsa_bitbucket id_rsa_github id_rsa_gitlab id_rsa_obsdams
+keychain --nogui id_ed25519_bitbucket id_rsa_github id_rsa_gitlab id_rsa_obsdams
 [ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
