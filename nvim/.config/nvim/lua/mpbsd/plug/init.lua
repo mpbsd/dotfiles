a1117e61bac66635be649c30588b398c78993d50
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-01-10 20:48:01 -0300

    start refactoring my neovim config
    specifically, my lsp config

--- nvim/.config/nvim/lua/mpbsd/plug/init.lua
+++ nvim/.config/nvim/lua/mpbsd/plug/init.lua
@@ -2,21 +2,31 @@ local globals = require('mpbsd.core.opts.globals')
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not globals.vim_uv.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+  vim.fn.system(
+    {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable',
+      lazypath,
+    }
+  )
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup(
   {
-    import = 'mpbsd.plug.pkgs',
+    {
+      import = 'mpbsd.plug.pkgs'
+    },
+    {
+      import = 'mpbsd.plug.pkgs.dap'
+    },
+    {
+      import = 'mpbsd.plug.pkgs.lsp'
+    },
   },
   {
     checker = {
