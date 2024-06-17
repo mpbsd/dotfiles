7162a6868af216e95c7baa5f7cdf7e078c670c5f
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-06-17 11:56:41 -0300

    rename my ssh keys
    algorithms now default to ed25519

--- zsh/.config/zsh/rc/keychain.zsh
+++ zsh/.config/zsh/rc/keychain.zsh
@@ -1,13 +1,4 @@
-if [ "$(uname -s)" = 'Linux' ]
-then
-  keychain --nogui id_ed25519_bitbucket id_ed25519_github id_ed25519_vultr
-  [ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
-  [ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
-  [ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
-elif [ "$(uname -s)" = 'Darwin' ]
-then
-  keychain --nogui bitbucket github vultr
-  [ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
-  [ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
-  [ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
-fi
+keychain --nogui bitbucket github vultr
+[ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
+[ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
+[ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
