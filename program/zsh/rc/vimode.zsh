2086b631c4fb04f3246bcea9383456bdbffe7c8e
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-10-09 19:30:00 -0300

    change cursor into an underline while in cmd mode in zsh

--- program/zsh/rc/vimode.zsh
+++ program/zsh/rc/vimode.zsh
@@ -47,10 +47,10 @@ bindkey -M vicmd '^e' edit-command-line
 function zle-keymap-select () {
   case "$KEYMAP" in
     vicmd)
-      echo -ne '\e[5 q'
+      echo -ne '\e[4 q'
       ;;
     viins|main)
-      echo -ne '\e[1 q'
+      echo -ne '\e[2 q'
       ;;
   esac
 }
