8ca52207a9e841bfe2e065c0d6fd928db1f582c4
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-10-13 08:43:57 -0300

    add colors to prompt

--- program/zsh/rc/prompt.zsh
+++ program/zsh/rc/prompt.zsh
@@ -1,9 +1,9 @@
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' formats "(%b) %m%u%c"
+zstyle ':vcs_info:*' formats "(%F{magenta}%b%f) %F{red}%u%f%F{yellow}%c%f"
 precmd() { vcs_info }
 
 setopt PROMPT_SUBST
 PROMPT='
-${PWD/#$HOME/~} ${vcs_info_msg_0_}
+%F{cyan}%~%f ${vcs_info_msg_0_}
 $ '
