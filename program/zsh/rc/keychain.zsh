2be8379db7e616da6a4dd8ffecbfcde40e776d5f
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-02-02 18:39:38 -0300

    add ssh key to access my raspberry pi

--- program/zsh/rc/keychain.zsh
+++ program/zsh/rc/keychain.zsh
@@ -1,4 +1,4 @@
-keychain --quiet --nogui bitbucket github home vultr
+keychain --quiet --nogui bitbucket github home vultr rpios
 [ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
