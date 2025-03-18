82780b5c9291350c5d53a58ca68f5f86072cef53
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-03-17 21:52:07 -0300

    format keybindings for increased legibility

--- program/awesome/rc.lua
+++ program/awesome/rc.lua
@@ -172,156 +172,534 @@ root.buttons(libs.gears.table.join(
 
 -- {{{ Key bindings
 local globalkeys = libs.gears.table.join(
-	libs.awful.key({ vars.modkey }, "s", libs.hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
-	libs.awful.key({ vars.modkey }, "Left", libs.awful.tag.viewprev, { description = "view previous", group = "tag" }),
-	libs.awful.key({ vars.modkey }, "Right", libs.awful.tag.viewnext, { description = "view next", group = "tag" }),
 	libs.awful.key(
-		{ vars.modkey },
+		{
+			vars.modkey,
+		},
+		"s",
+		libs.hotkeys_popup.show_help,
+		{
+			description = "show help",
+			group = "awesome",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"Left",
+		libs.awful.tag.viewprev,
+		{
+			description = "view previous",
+			group = "tag",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"Right",
+		libs.awful.tag.viewnext,
+		{
+			description = "view next",
+			group = "tag",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
 		"Escape",
 		libs.awful.tag.history.restore,
-		{ description = "go back", group = "tag" }
+		{
+			description = "go back",
+			group = "tag",
+		}
 	),
 
-	libs.awful.key({ vars.modkey }, "j", function()
-		libs.awful.client.focus.byidx(1)
-	end, { description = "focus next by index", group = "client" }),
-	libs.awful.key({ vars.modkey }, "k", function()
-		libs.awful.client.focus.byidx(-1)
-	end, { description = "focus previous by index", group = "client" }),
-	libs.awful.key({ vars.modkey }, "w", function()
-		mymainmenu:show()
-	end, { description = "show main menu", group = "awesome" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"j",
+		function()
+			libs.awful.client.focus.byidx(1)
+		end,
+		{
+			description = "focus next by index",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"k",
+		function()
+			libs.awful.client.focus.byidx(-1)
+		end,
+		{
+			description = "focus previous by index",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"w",
+		function()
+			mymainmenu:show()
+		end,
+		{
+			description = "show main menu",
+			group = "awesome",
+		}
+	),
 
 	-- Layout manipulation
-	libs.awful.key({ vars.modkey, "Shift" }, "j", function()
-		libs.awful.client.swap.byidx(1)
-	end, { description = "swap with next client by index", group = "client" }),
-	libs.awful.key({ vars.modkey, "Shift" }, "k", function()
-		libs.awful.client.swap.byidx(-1)
-	end, { description = "swap with previous client by index", group = "client" }),
-	libs.awful.key({ vars.modkey, "Control" }, "j", function()
-		libs.awful.screen.focus_relative(1)
-	end, { description = "focus the next screen", group = "screen" }),
-	libs.awful.key({ vars.modkey, "Control" }, "k", function()
-		libs.awful.screen.focus_relative(-1)
-	end, { description = "focus the previous screen", group = "screen" }),
 	libs.awful.key(
-		{ vars.modkey },
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"j",
+		function()
+			libs.awful.client.swap.byidx(1)
+		end,
+		{
+			description = "swap with next client by index",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"k",
+		function()
+			libs.awful.client.swap.byidx(-1)
+		end,
+		{
+			description = "swap with previous client by index",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"j",
+		function()
+			libs.awful.screen.focus_relative(1)
+		end,
+		{
+			description = "focus the next screen",
+			group = "screen",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"k",
+		function()
+			libs.awful.screen.focus_relative(-1)
+		end,
+		{
+			description = "focus the previous screen",
+			group = "screen",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
 		"u",
 		libs.awful.client.urgent.jumpto,
-		{ description = "jump to urgent client", group = "client" }
+		{
+			description = "jump to urgent client",
+			group = "client",
+		}
 	),
-	libs.awful.key({ vars.modkey }, "Tab", function()
-		libs.awful.client.focus.history.previous()
-		if client.focus then
-			client.focus:raise()
-		end
-	end, { description = "go back", group = "client" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"Tab",
+		function()
+			libs.awful.client.focus.history.previous()
+			if client.focus then
+				client.focus:raise()
+			end
+		end,
+		{
+			description = "go back",
+			group = "client",
+		}
+	),
 
 	-- Standard program
-	libs.awful.key({ vars.modkey, "Shift" }, "Return", function()
-		libs.awful.spawn(vars.terminal)
-	end, { description = "open a terminal", group = "launcher" }),
 	libs.awful.key(
-		{ vars.modkey, "Control" },
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"Return",
+		function()
+			libs.awful.spawn(vars.terminal)
+		end,
+		{
+			description = "open a terminal",
+			group = "launcher",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
 		"r",
 		awesome.restart,
-		{ description = "reload awesome", group = "awesome" }
+		{
+			description = "reload awesome",
+			group = "awesome",
+		}
 	),
-	libs.awful.key({ vars.modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"q",
+		awesome.quit,
+		{
+			description = "quit awesome",
+			group = "awesome",
+		}
+	),
 
-	libs.awful.key({ vars.modkey }, "l", function()
-		libs.awful.tag.incmwfact(0.05)
-	end, { description = "increase master width factor", group = "layout" }),
-	libs.awful.key({ vars.modkey }, "h", function()
-		libs.awful.tag.incmwfact(-0.05)
-	end, { description = "decrease master width factor", group = "layout" }),
-	libs.awful.key({ vars.modkey, "Shift" }, "h", function()
-		libs.awful.tag.incnmaster(1, nil, true)
-	end, { description = "increase the number of master clients", group = "layout" }),
-	libs.awful.key({ vars.modkey, "Shift" }, "l", function()
-		libs.awful.tag.incnmaster(-1, nil, true)
-	end, { description = "decrease the number of master clients", group = "layout" }),
-	libs.awful.key({ vars.modkey, "Control" }, "h", function()
-		libs.awful.tag.incncol(1, nil, true)
-	end, { description = "increase the number of columns", group = "layout" }),
-	libs.awful.key({ vars.modkey, "Control" }, "l", function()
-		libs.awful.tag.incncol(-1, nil, true)
-	end, { description = "decrease the number of columns", group = "layout" }),
-	libs.awful.key({ vars.modkey }, "space", function()
-		libs.awful.layout.inc(1)
-	end, { description = "select next", group = "layout" }),
-	libs.awful.key({ vars.modkey, "Shift" }, "space", function()
-		libs.awful.layout.inc(-1)
-	end, { description = "select previous", group = "layout" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"l",
+		function()
+			libs.awful.tag.incmwfact(0.05)
+		end,
+		{
+			description = "increase master width factor",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"h",
+		function()
+			libs.awful.tag.incmwfact(-0.05)
+		end,
+		{
+			description = "decrease master width factor",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"h",
+		function()
+			libs.awful.tag.incnmaster(1, nil, true)
+		end,
+		{
+			description = "increase the number of master clients",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"l",
+		function()
+			libs.awful.tag.incnmaster(-1, nil, true)
+		end,
+		{
+			description = "decrease the number of master clients",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"h",
+		function()
+			libs.awful.tag.incncol(1, nil, true)
+		end,
+		{
+			description = "increase the number of columns",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"l",
+		function()
+			libs.awful.tag.incncol(-1, nil, true)
+		end,
+		{
+			description = "decrease the number of columns",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"space",
+		function()
+			libs.awful.layout.inc(1)
+		end,
+		{
+			description = "select next",
+			group = "layout",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"space",
+		function()
+			libs.awful.layout.inc(-1)
+		end,
+		{
+			description = "select previous",
+			group = "layout",
+		}
+	),
 
-	libs.awful.key({ vars.modkey, "Control" }, "n", function()
-		local c = libs.awful.client.restore()
-		-- Focus restored client
-		if c then
-			c:emit_signal("request::activate", "key.unminimize", { raise = true })
-		end
-	end, { description = "restore minimized", group = "client" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"n",
+		function()
+			local c = libs.awful.client.restore()
+			-- Focus restored client
+			if c then
+				c:emit_signal("request::activate", "key.unminimize", { raise = true })
+			end
+		end,
+		{
+			description = "restore minimized",
+			group = "client",
+		}
+	),
 
 	-- Prompt
-	libs.awful.key({ vars.modkey }, "r", function()
-		libs.awful.screen.focused().mypromptbox:run()
-	end, { description = "run prompt", group = "launcher" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"r",
+		function()
+			libs.awful.screen.focused().mypromptbox:run()
+		end,
+		{
+			description = "run prompt",
+			group = "launcher",
+		}
+	),
 
-	libs.awful.key({ vars.modkey }, "x", function()
-		libs.awful.prompt.run({
-			prompt = "Run Lua code: ",
-			textbox = libs.awful.screen.focused().mypromptbox.widget,
-			exe_callback = libs.awful.util.eval,
-			history_path = libs.awful.util.get_cache_dir() .. "/history_eval",
-		})
-	end, { description = "lua execute prompt", group = "awesome" }),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"x",
+		function()
+			libs.awful.prompt.run({
+				prompt = "Run Lua code: ",
+				textbox = libs.awful.screen.focused().mypromptbox.widget,
+				exe_callback = libs.awful.util.eval,
+				history_path = libs.awful.util.get_cache_dir() .. "/history_eval",
+			})
+		end,
+		{
+			description = "lua execute prompt",
+			group = "awesome",
+		}
+	),
 	-- libs.Menubar
-	libs.awful.key({ vars.modkey }, "p", function()
-		libs.awful.spawn("dmenu_run")
-	end, { description = "show the libs.menubar", group = "launcher" })
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"p",
+		function()
+			libs.awful.spawn("dmenu_run")
+		end,
+		{
+			description = "show the libs.menubar",
+			group = "launcher",
+		}
+	)
 )
 
 local clientkeys = libs.gears.table.join(
-	libs.awful.key({ vars.modkey }, "f", function(c)
-		c.fullscreen = not c.fullscreen
-		c:raise()
-	end, { description = "toggle fullscreen", group = "client" }),
-	libs.awful.key({ vars.modkey, "Shift" }, "c", function(c)
-		c:kill()
-	end, { description = "close", group = "client" }),
 	libs.awful.key(
-		{ vars.modkey, "Control" },
+		{
+			vars.modkey,
+		},
+		"f",
+		function(c)
+			c.fullscreen = not c.fullscreen
+			c:raise()
+		end,
+		{
+			description = "toggle fullscreen",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"c",
+		function(c)
+			c:kill()
+		end,
+		{
+			description = "close",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
 		"space",
 		libs.awful.client.floating.toggle,
-		{ description = "toggle floating", group = "client" }
+		{
+			description = "toggle floating",
+			group = "client",
+		}
 	),
-	libs.awful.key({ vars.modkey, "Control" }, "Return", function(c)
-		c:swap(libs.awful.client.getmaster())
-	end, { description = "move to master", group = "client" }),
-	libs.awful.key({ vars.modkey }, "o", function(c)
-		c:move_to_screen()
-	end, { description = "move to screen", group = "client" }),
-	libs.awful.key({ vars.modkey }, "t", function(c)
-		c.ontop = not c.ontop
-	end, { description = "toggle keep on top", group = "client" }),
-	libs.awful.key({ vars.modkey }, "n", function(c)
-		-- The client currently has the input focus, so it cannot be
-		-- minimized, since minimized clients can't have the focus.
-		c.minimized = true
-	end, { description = "minimize", group = "client" }),
-	libs.awful.key({ vars.modkey }, "m", function(c)
-		c.maximized = not c.maximized
-		c:raise()
-	end, { description = "(un)maximize", group = "client" }),
-	libs.awful.key({ vars.modkey, "Control" }, "m", function(c)
-		c.maximized_vertical = not c.maximized_vertical
-		c:raise()
-	end, { description = "(un)maximize vertically", group = "client" }),
-	libs.awful.key({ vars.modkey, "Shift" }, "m", function(c)
-		c.maximized_horizontal = not c.maximized_horizontal
-		c:raise()
-	end, { description = "(un)maximize horizontally", group = "client" })
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"Return",
+		function(c)
+			c:swap(libs.awful.client.getmaster())
+		end,
+		{
+			description = "move to master",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"o",
+		function(c)
+			c:move_to_screen()
+		end,
+		{
+			description = "move to screen",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"t",
+		function(c)
+			c.ontop = not c.ontop
+		end,
+		{
+			description = "toggle keep on top",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"n",
+		function(c)
+			-- The client currently has the input focus, so it cannot be
+			-- minimized, since minimized clients can't have the focus.
+			c.minimized = true
+		end,
+		{
+			description = "minimize",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+		},
+		"m",
+		function(c)
+			c.maximized = not c.maximized
+			c:raise()
+		end,
+		{
+			description = "(un)maximize",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Control",
+		},
+		"m",
+		function(c)
+			c.maximized_vertical = not c.maximized_vertical
+			c:raise()
+		end,
+		{
+			description = "(un)maximize vertically",
+			group = "client",
+		}
+	),
+	libs.awful.key(
+		{
+			vars.modkey,
+			"Shift",
+		},
+		"m",
+		function(c)
+			c.maximized_horizontal = not c.maximized_horizontal
+			c:raise()
+		end,
+		{
+			description = "(un)maximize horizontally",
+			group = "client",
+		}
+	)
 )
 
 -- Bind all key numbers to tags.
@@ -331,39 +709,83 @@ for i = 1, 9 do
 	globalkeys = libs.gears.table.join(
 		globalkeys,
 		-- View tag only.
-		libs.awful.key({ vars.modkey }, "#" .. i + 9, function()
-			local screen = libs.awful.screen.focused()
-			local tag = screen.tags[i]
-			if tag then
-				tag:view_only()
-			end
-		end, { description = "view tag #" .. i, group = "tag" }),
+		libs.awful.key(
+			{
+				vars.modkey,
+			},
+			"#" .. i + 9,
+			function()
+				local screen = libs.awful.screen.focused()
+				local tag = screen.tags[i]
+				if tag then
+					tag:view_only()
+				end
+			end,
+			{
+				description = "view tag #" .. i,
+				group = "tag",
+			}
+		),
 		-- Toggle tag display.
-		libs.awful.key({ vars.modkey, "Control" }, "#" .. i + 9, function()
-			local screen = libs.awful.screen.focused()
-			local tag = screen.tags[i]
-			if tag then
-				libs.awful.tag.viewtoggle(tag)
-			end
-		end, { description = "toggle tag #" .. i, group = "tag" }),
+		libs.awful.key(
+			{
+				vars.modkey,
+				"Control",
+			},
+			"#" .. i + 9,
+			function()
+				local screen = libs.awful.screen.focused()
+				local tag = screen.tags[i]
+				if tag then
+					libs.awful.tag.viewtoggle(tag)
+				end
+			end,
+			{
+				description = "toggle tag #" .. i,
+				group = "tag",
+			}
+		),
 		-- Move client to tag.
-		libs.awful.key({ vars.modkey, "Shift" }, "#" .. i + 9, function()
-			if client.focus then
-				local tag = client.focus.screen.tags[i]
-				if tag then
-					client.focus:move_to_tag(tag)
+		libs.awful.key(
+			{
+				vars.modkey,
+				"Shift",
+			},
+			"#" .. i + 9,
+			function()
+				if client.focus then
+					local tag = client.focus.screen.tags[i]
+					if tag then
+						client.focus:move_to_tag(tag)
+					end
 				end
-			end
-		end, { description = "move focused client to tag #" .. i, group = "tag" }),
+			end,
+			{
+				description = "move focused client to tag #" .. i,
+				group = "tag",
+			}
+		),
 		-- Toggle tag on focused client.
-		libs.awful.key({ vars.modkey, "Control", "Shift" }, "#" .. i + 9, function()
-			if client.focus then
-				local tag = client.focus.screen.tags[i]
-				if tag then
-					client.focus:toggle_tag(tag)
+		libs.awful.key(
+			{
+				vars.modkey,
+				"Control",
+				"Shift",
+			},
+			"#" .. i + 9,
+			function()
+				if client.focus then
+					local tag = client.focus.screen.tags[i]
+					if tag then
+						client.focus:toggle_tag(tag)
+					end
 				end
-			end
-		end, { description = "toggle focused client on tag #" .. i, group = "tag" })
+			end,
+			{
+				description = "toggle focused client on tag #" .. i,
+				group = "tag",
+			}
+		)
 	)
 end
 
