a94e0aeba8684978f5ef956997325b2351a0aea1
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-03-22 17:45:55 -0300

    one step ahead

--- program/awesome/rc.lua
+++ program/awesome/rc.lua
@@ -164,14 +164,11 @@ root.buttons(mods.gears.table.join(
 -- }}}
 
 -- Key bindings {{{
-local globalkeys = keys["globalkeys"]
+-- local globalkeys = keys["globalkeys"]
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 9 do
-	globalkeys = mods.gears.table.join(
-		globalkeys,
+local myfunc = function(i)
+	local M = mods.gears.table.join(
+		keys["globalkeys"],
 		-- View tag only.
 		mods.awful.key(
 			{
@@ -251,6 +248,92 @@ for i = 1, 9 do
 			}
 		)
 	)
+	return M
+end
+
+for i = 1, 9 do
+	globalkeys = myfunc(i)
+	-- 	globalkeys = mods.gears.table.join(
+	-- 		globalkeys,
+	-- 		-- View tag only.
+	-- 		mods.awful.key(
+	-- 			{
+	-- 				vars.modkey,
+	-- 			},
+	-- 			"#" .. i + 9,
+	-- 			function()
+	-- 				local screen = mods.awful.screen.focused()
+	-- 				local tag = screen.tags[i]
+	-- 				if tag then
+	-- 					tag:view_only()
+	-- 				end
+	-- 			end,
+	-- 			{
+	-- 				description = "view tag #" .. i,
+	-- 				group = "tag",
+	-- 			}
+	-- 		),
+	-- 		-- Toggle tag display.
+	-- 		mods.awful.key(
+	-- 			{
+	-- 				vars.modkey,
+	-- 				"Control",
+	-- 			},
+	-- 			"#" .. i + 9,
+	-- 			function()
+	-- 				local screen = mods.awful.screen.focused()
+	-- 				local tag = screen.tags[i]
+	-- 				if tag then
+	-- 					mods.awful.tag.viewtoggle(tag)
+	-- 				end
+	-- 			end,
+	-- 			{
+	-- 				description = "toggle tag #" .. i,
+	-- 				group = "tag",
+	-- 			}
+	-- 		),
+	-- 		-- Move client to tag.
+	-- 		mods.awful.key(
+	-- 			{
+	-- 				vars.modkey,
+	-- 				"Shift",
+	-- 			},
+	-- 			"#" .. i + 9,
+	-- 			function()
+	-- 				if client.focus then
+	-- 					local tag = client.focus.screen.tags[i]
+	-- 					if tag then
+	-- 						client.focus:move_to_tag(tag)
+	-- 					end
+	-- 				end
+	-- 			end,
+	-- 			{
+	-- 				description = "move focused client to tag #" .. i,
+	-- 				group = "tag",
+	-- 			}
+	-- 		),
+	-- 		-- Toggle tag on focused client.
+	-- 		mods.awful.key(
+	-- 			{
+	-- 				vars.modkey,
+	-- 				"Control",
+	-- 				"Shift",
+	-- 			},
+	-- 			"#" .. i + 9,
+	-- 			function()
+	-- 				if client.focus then
+	-- 					local tag = client.focus.screen.tags[i]
+	-- 					if tag then
+	-- 						client.focus:toggle_tag(tag)
+	-- 					end
+	-- 				end
+	-- 			end,
+	-- 			{
+	-- 				description = "toggle focused client on tag #" .. i,
+	-- 				group = "tag",
+	-- 			}
+	-- 		)
+	-- 	)
 end
 
 -- Set keys
