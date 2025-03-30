a610051207a40e89115adc29bba24c436c6ba625
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-03-30 18:36:11 -0300

    minor tweaks here and there

--- program/zsh/rc/keychain.zsh
+++ program/zsh/rc/keychain.zsh
@@ -1,4 +1,4 @@
-keychain --quiet --nogui obsdams pass github
+keychain --quiet --nogui obsdams pass github chros
 [ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
