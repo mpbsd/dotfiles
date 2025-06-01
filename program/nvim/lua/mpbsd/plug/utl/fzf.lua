fb4c8a1594d3b8f3d862cef706b17697a0444bdc
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-05-31 23:25:25 -0300

    add telescope-bibtex.nvim

--- program/nvim/lua/mpbsd/plug/utl/fzf.lua
+++ program/nvim/lua/mpbsd/plug/utl/fzf.lua
@@ -7,11 +7,13 @@ return {
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
 		},
+		"nvim-telescope/telescope-bibtex.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
 		local actions = require("telescope.actions")
+		local bibtex_actions = require("telescope-bibtex.actions")
 		local MAP = require("mpbsd.core.glb").MAP
 
 		telescope.setup({
@@ -36,10 +38,46 @@ return {
 					override_file_sorter = true,
 					case_mode = "smart_case",
 				},
+				bibtex = {
+					-- Depth for the *.bib file
+					depth = 1,
+					-- Custom format for citation label
+					custom_formats = {},
+					-- Format to use for citation label.
+					-- Try to match the filetype by default, or use 'plain'
+					format = "",
+					-- Path to global bibliographies (placed outside of the project)
+					global_files = { "~/.local/share/bibliography/full.bib" },
+					-- Define the search keys to use in the picker
+					search_keys = { "author", "year", "title" },
+					-- Template for the formatted citation
+					citation_format = "{{author}} ({{year}}), {{title}}.",
+					-- Only use initials for the authors first name
+					citation_trim_firstname = true,
+					-- Max number of authors to write in the formatted citation
+					-- following authors will be replaced by "et al."
+					citation_max_auth = 2,
+					-- Context awareness disabled by default
+					context = false,
+					-- Fallback to global/directory .bib files if context not found
+					-- This setting has no effect if context = false
+					context_fallback = true,
+					-- Wrapping in the preview window is disabled by default
+					wrap = false,
+					-- user defined mappings
+					mappings = {
+						i = {
+							["<CR>"] = bibtex_actions.key_append("%s"), -- format is determined by filetype if the user has not set it explictly
+							["<C-e>"] = bibtex_actions.entry_append,
+							["<C-c>"] = bibtex_actions.citation_append("{{author}} ({{year}}), {{title}}."),
+						},
+					},
+				},
 			},
 		})
 
 		telescope.load_extension("fzf")
+		telescope.load_extension("bibtex")
 
 		local KEY = {
 			-- [F]ind [F]iles {{{
