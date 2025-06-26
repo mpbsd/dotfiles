156c0a9494c1fdf9bbc831b8a377dd781cc76b43
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-06-26 07:37:00 -0300

    rename ssh keys

--- program/zsh/rc/keychain.zsh
+++ program/zsh/rc/keychain.zsh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 keychain --quiet --nogui obsd pass home ghub cros
-=======
-keychain --quiet --nogui obsd pass ghub cros
->>>>>>> 8be2cea (rename ssh keys)
 [ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh ] && . ${HOME}/.keychain/${HOSTNAME}-sh
 [ -f ${HOME}/.keychain/${HOSTNAME}-sh-gpg ] && . ${HOME}/.keychain/${HOSTNAME}-sh-gpg
