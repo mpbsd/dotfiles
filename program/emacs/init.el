e9199df4906a3a603cf058940770321ce622e99f
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-12-25 16:23:53 -0300

    fix a flaw in my aux layer
    It was in my hhk.kbd layout; all 6 macros were pointing to the same
    sequence of keys.

--- program/emacs/init.el
+++ program/emacs/init.el
@@ -1,5 +1,52 @@
 (setq vc-follow-symlinks t)
-(org-babel-load-file (expand-file-name "evil.org" user-emacs-directory))
+
+(setq gc-cons-threshold (* 8 1024 1024 1024))
+(run-with-idle-timer 10 t (lambda () (garbage-collect)))
+(setq read-process-output-max (* 1024 1024))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+
+(defvar backups-dir
+  (file-name-as-directory (expand-file-name "backups" user-emacs-directory)))
+(unless (file-exists-p backups-dir)
+  (make-directory backups-dir))
+
+(setq backup-by-copying t
+      backup-directory-alist `((".*" . ,backups-dir))
+      delete-old-version t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
+(setq frame-resize-pixelwise t)
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+(global-hl-line-mode)
+
+(defvar +font-size-fixed 128)
+(defvar +font-size-variable 128)
+(set-face-attribute 'default nil
+                    :family "JetBrainsMono Nerd Font Mono"
+                    :height +font-size-fixed
+                    :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+                    :family "JetBrainsMono Nerd Font Mono"
+                    :height +font-size-fixed
+                    :weight 'medium)
+(set-face-attribute 'variable-pitch nil
+                    :family "JetBrainsMono Nerd Font Mono"
+                    :height +font-size-variable
+                    :weight 'medium)
+
+(load-theme 'tango-dark)
