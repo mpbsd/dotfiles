28b6087c00581e426ca240e08df9baeba51cf0fa
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2025-12-31 18:17:57 -0300

    much cleaner ;)

--- program/zsh/rc/keychain.zsh
+++ program/zsh/rc/keychain.zsh
@@ -1,4 +1,14 @@
-keychain --absolute --dir "${XDG_RUNTIME_DIR}/keychain" --quiet --nogui obsd pass home ghub cros
-[ -z "${HOSTNAME}" ] && HOSTNAME=`uname -n`
-[ -f "${XDG_RUNTIME_DIR}/.keychain/${HOSTNAME}-sh" ] && . "${XDG_RUNTIME_DIR}/.keychain/${HOSTNAME}-sh"
-[ -f "${XDG_RUNTIME_DIR}/.keychain/${HOSTNAME}-sh-gpg" ] && . "${XDG_RUNTIME_DIR}/.keychain/${HOSTNAME}-sh-gpg"
+typeset -a KEYS=(
+  obsd
+  pass
+  home
+  ghub
+  cros
+)
+
+keychain \
+  --absolute \
+  --dir "${XDG_RUNTIME_DIR}/keychain}" \
+  --nogui \
+  --quiet \
+  "${(z)KEYS}"
