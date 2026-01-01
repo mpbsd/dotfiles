b9159bd6cd8e5d01da01f3808294c7c7494d1da5
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2026-01-01 13:35:08 -0300

    nice scheme of colors for my Zsh prompt

--- program/zsh/rc/prompt.zsh
+++ program/zsh/rc/prompt.zsh
@@ -3,11 +3,11 @@ autoload -Uz vcs_info
 precmd() { vcs_info }
 
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr '%F{green}●%f'
-zstyle ':vcs_info:*' unstagedstr '%F{red}●%f'
-zstyle ':vcs_info:*' formats '%c%u(%F{yellow}%b%f)'
+zstyle ':vcs_info:*' stagedstr '%F{#93A1A1}●%f'
+zstyle ':vcs_info:*' unstagedstr '%F{#A19393}●%f'
+zstyle ':vcs_info:*' formats '%c%u (%F{#758080}%b%f)'
 
-PROMPT='%F{red}❯%f%F{yellow}❯%f%F{green}❯%f '
-RPROMPT='${vcs_info_msg_0_} %F{cyan}%~%f'
+PROMPT='%F{#667070}❯%f%F{#758080}❯%f%F{#849090}❯%f '
+RPROMPT='${vcs_info_msg_0_} %F{#758080}%~%f'
 
 setopt prompt_subst
