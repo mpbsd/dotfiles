0548282ba44cb5f8cd2a984616f0fcfc102f68ee
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2026-01-07 11:49:49 -0300

    brighter fg at rprompt

--- program/zsh/rc/prompt.zsh
+++ program/zsh/rc/prompt.zsh
@@ -5,9 +5,9 @@ precmd() { vcs_info }
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr '%F{#93A1A1}●%f'
 zstyle ':vcs_info:*' unstagedstr '%F{#A19393}●%f'
-zstyle ':vcs_info:*' formats '%c%u (%F{#758080}%b%f)'
+zstyle ':vcs_info:*' formats '%c%u (%F{#849090}%b%f)'
 
 PROMPT='%F{#667070}❯%f%F{#758080}❯%f%F{#849090}❯%f '
-RPROMPT='${vcs_info_msg_0_} %F{#758080}%~%f'
+RPROMPT='${vcs_info_msg_0_} %F{#849090}%~%f'
 
 setopt prompt_subst
